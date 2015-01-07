4764eb0867f7402f8d7d80328ec7c26348090cb0
dansan <daniel@admin-box.com>
2015-01-07 12:04:00 +0100

    use logging in SMS cli

--- notify_sms/cli.py
+++ notify_sms/cli.py
@@ -20,7 +20,7 @@ english: http://www.sipgate.de/beta/public/static/downloads/basic/api/sipgate_ap
 
 import sys
 import os
-import traceback
+import logging
 
 from argparse import ArgumentParser
 from argparse import RawDescriptionHelpFormatter
@@ -33,6 +33,8 @@ __updated__ = '2015-01-07'
 
 DEBUG = 0
 
+logger = logging.getLogger()
+
 
 def main(argv=None):  # IGNORE:C0111
     '''Send SMS as specified on the command line.'''
@@ -42,12 +44,13 @@ def main(argv=None):  # IGNORE:C0111
     else:
         sys.argv.extend(argv)
     args = parse_cmd_line()
+    setup_logging(args)
+
     try:
-        ssms = Sipgate_SMS(args.username, args.password, bool(args.verbose))
-        ssms.send(args.destination, args.message)
+        ssms = Sipgate_SMS(args.username, args.password)
+        ssms.send(args.mobile, args.message)
     except:
-        print "Could not send message:\n"
-        traceback.print_exc(file=sys.stdout)
+        logger.exception("Could not send message:\n")
         return 1
 
 
@@ -76,7 +79,7 @@ USAGE
         parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
         parser.add_argument("username", help="sipgate account username")
         parser.add_argument("password", help="sipgate account password")
-        parser.add_argument("destination", help="number to send SMS to")
+        parser.add_argument("mobile", help="mobile phone number to send SMS to")
         parser.add_argument("message", help="the message, max 160 characters")
         parser.add_argument("-v", "--verbose", dest="verbose", action="count",
                             help="set verbosity level [default: %(default)s]")
@@ -86,7 +89,7 @@ USAGE
         args = parser.parse_args()
 
         if len(args.message) > 160:
-            print "Message to long."
+            logger.error("Message to long.")
 
     except KeyboardInterrupt:
         # handle keyboard interrupt
@@ -95,11 +98,24 @@ USAGE
         if DEBUG:
             raise(e)
         indent = len(program_name) * " "
-        sys.stderr.write(program_name + ": " + repr(e) + "\n")
-        sys.stderr.write(indent + "  for help use --help")
+        logger.error(program_name + ": " + repr(e))
+        logger.error(indent + "  for help use --help")
         return 2
 
     return args
+
+def setup_logging(args):
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    if args.verbose:
+        ch.setLevel(logging.DEBUG)
+    else:
+        ch.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        fmt='%(asctime)s %(levelname)-5s %(module)s.%(funcName)s:%(lineno)d  %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+    logger.addHandler(ch)
+
 
 if __name__ == "__main__":
     if DEBUG:
